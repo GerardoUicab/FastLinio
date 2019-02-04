23bc1a4bddbefcb1f5b19af904c7a809ff2e9bd7
Gerardo Uicab <electro.gospel277@gmail.com>
2019-02-04 14:57:07 -0600

    holitas del mar

---
+++ UsuarioCliente/Admin/index.php
@@ -0,0 +1,162 @@
+<?php include '../Admin/MasterPage/header.php' ?>
+<div class="inner_content">
+				    <!-- /inner_content_w3_agile_info-->
+					<div class="inner_content_w3_agile_info">
+					<!-- /agile_top_w3_grids-->
+					   <div class="agile_top_w3_grids">
+					          <ul class="ca-menu">
+									<li>
+										<a href="#">
+											
+											<i class="fa fa-building-o" aria-hidden="true"></i>
+											<div class="ca-content">
+												<h4 class="ca-main">16,808</h4>
+												<h3 class="ca-sub">New Companies</h3>
+											</div>
+										</a>
+									</li>
+									<li>
+										<a href="#">
+										  <i class="fa fa-user" aria-hidden="true"></i>
+											<div class="ca-content">
+												<h4 class="ca-main one">26,808</h4>
+												<h3 class="ca-sub one">New Clients</h3>
+											</div>
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<i class="fa fa-database" aria-hidden="true"></i>
+											<div class="ca-content">
+											<h4 class="ca-main two">29,008</h4>
+												<h3 class="ca-sub two">New Projects</h3>
+											</div>
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<i class="fa fa-tasks" aria-hidden="true"></i>
+											<div class="ca-content">
+												<h4 class="ca-main three">49,436</h4>
+												<h3 class="ca-sub three">Old Projects</h3>
+											</div>
+										</a>
+									</li>
+										<li>
+										<a href="#">
+											<i class="fa fa-clone" aria-hidden="true"></i>
+											<div class="ca-content">
+												<h4 class="ca-main four">30,808</h4>
+												<h3 class="ca-sub four">New Orders</h3>
+											</div>
+										</a>
+									</li>
+									<div class="clearfix"></div>
+								</ul>
+					   </div>
+						<!-- //weather_w3_agile_info-->
+						<!-- /social_media-->
+						  <div class="social_media_w3ls">
+						 
+						      <div class="col-md-3 socail_grid_agile facebook">
+                                     <ul class="icon_w3_info">
+									   <li><a href="#" class="wthree_facebook"> <i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									   <li>Facebook</li>
+									 </ul>
+									  <ul class="icon_w3_social">
+									    <li><i class="fa fa-comment-o" aria-hidden="true"></i></li>
+										<li><i class="fa fa-envelope-o" aria-hidden="true"></i></li>
+										<li><i class="fa fa-user" aria-hidden="true"></i></li>
+                                      </ul>
+									 <div class="clearfix"></div>
+									  <div class="bottom_info_social">
+									 <div class="col-md-4 agile_w3l_social_media_text_img">
+									    <img src="images/admin.jpg" alt="">
+									 </div>
+									  <div class="col-md-8 agile_w3l_social_media_text">
+									    <h4>Hurisa Joe</h4>
+										<p>Lorem ipsum dolor sit amet</p>
+									 </div>
+									
+									 <div class="clearfix"></div>
+									 </div>
+                              </div>
+							  <div class="col-md-3 socail_grid_agile twitter">
+                                         <ul class="icon_w3_info">
+									   <li><a href="#" class="wthree_facebook"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+									   <li>Twitter</li>
+									 </ul>
+									  <ul class="icon_w3_social">
+									    <li><i class="fa fa-comment-o" aria-hidden="true"></i></li>
+										<li><i class="fa fa-envelope-o" aria-hidden="true"></i></li>
+										<li><i class="fa fa-user" aria-hidden="true"></i></li>
+                                      </ul>
+									 <div class="clearfix"></div>
+									  <div class="bottom_info_social">
+									 <div class="col-md-4 agile_w3l_social_media_text_img">
+									    <img src="images/a1.jpg" alt="">
+									 </div>
+									  <div class="col-md-8 agile_w3l_social_media_text">
+									    <h4>Jasmin Joe</h4>
+										<p>Lorem ipsum dolor sit amet</p>
+									 </div>
+									
+									 <div class="clearfix"></div>
+									 </div>
+                              </div>
+							  <div class="col-md-3 socail_grid_agile gmail">
+                                     <ul class="icon_w3_info">
+									   <li><a href="#" class="wthree_facebook"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+									   <li>Google+</li>
+									 </ul>
+									  <ul class="icon_w3_social">
+									    <li><i class="fa fa-comment-o" aria-hidden="true"></i></li>
+										<li><i class="fa fa-envelope-o" aria-hidden="true"></i></li>
+										<li><i class="fa fa-user" aria-hidden="true"></i></li>
+                                      </ul>
+									 <div class="clearfix"></div>
+									  <div class="bottom_info_social">
+									 <div class="col-md-4 agile_w3l_social_media_text_img">
+									    <img src="images/a2.jpg" alt="">
+									 </div>
+									  <div class="col-md-8 agile_w3l_social_media_text">
+									    <h4>John Pal</h4>
+										<p>Lorem ipsum dolor sit amet</p>
+									 </div>
+									
+									 <div class="clearfix"></div>
+									 </div>
+                              </div>
+							  <div class="col-md-3 socail_grid_agile dribble">
+							  
+							     <ul class="icon_w3_info">
+									  <li><a href="#" class="wthree_facebook"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+									   <li>Dribbble</li>
+									 </ul>
+									  <ul class="icon_w3_social">
+									    <li><i class="fa fa-comment-o" aria-hidden="true"></i></li>
+										<li><i class="fa fa-envelope-o" aria-hidden="true"></i></li>
+										<li><i class="fa fa-user" aria-hidden="true"></i></li>
+                                      </ul>
+									 <div class="clearfix"></div>
+									  <div class="bottom_info_social">
+									 <div class="col-md-4 agile_w3l_social_media_text_img">
+									    <img src="images/a4.jpg" alt="">
+									 </div>
+									  <div class="col-md-8 agile_w3l_social_media_text">
+									    <h4>Honey Pal</h4>
+										<p>Lorem ipsum dolor sit amet</p>
+									 </div>
+									
+									 <div class="clearfix"></div>
+									 </div>
+
+                              </div>
+							  <div class="clearfix"></div>
+													
+						</div>
+						<!-- //social_media-->
+				    </div>
+					<!-- //inner_content_w3_agile_info-->
+</div>
+<?php include '../Admin/MasterPage/footer.php' ?>
