848294a000936135618cd0edce608f17b7894cd2
Gerardo Uicab <electro.gospel277@gmail.com>
2019-02-04 10:47:38 -0600

    Ortografia

--- UsuarioCliente/index.php
+++ UsuarioCliente/index.php
@@ -46,7 +46,7 @@
 			</div>
 	</div>
 	<!-- ==================================== 
-	Contenedor Slider 
+	aqui esta la descripcion de que es la pagina
 	=======================================-->
 	<section id="slider" class="container">
 		<ul class="slider-wrapper">
@@ -67,33 +67,11 @@
 		<ul id="control-buttons" class="control-buttons"></ul>
 	</section>
 	
-	
-	<!-- Imagenes Copyright -->
-	<<!--div class="container">
-	<div class="carousel right">
-  <div class="indicator"></div>
-  <div class="wrap">
-    <ul>
-      <li><img src="http://ppcdn.500px.org/23718959/f216a69d1cb7667436a1e6a33c7e4b2894d0fe18/4.jpg" title="50% DE DESCUENTO APROVECHE"/><button type="button" class="btn btn-primary">Primary</button></li>
-      <li><img src="http://ppcdn.500px.org/72233281/5e145f8f95427805045dbaf1ec2cf17bcb4b25b3/3.jpg"/></li>
-      <li><img src="http://ppcdn.500px.org/72190239/1ac03d25646f92afd4d80ed8e6f2c1cfc78635b5/4.jpg"/></li>
-      <li><img src="http://ppcdn.500px.org/58739696/b0c33312552e8581ef457720bb924f16421dcec8/3.jpg"/></li>
-      <li><img src="http://ppcdn.500px.org/43028892/84b059f25810f07962ae46e63c44c625b2f9b9e6/2048.jpg"/></li>
-      <li><img src="http://ppcdn.500px.org/33154839/a53aeaca99ee94a9803f445fd30163142f308829/3.jpg"/></li>
-      <li><img src="http://ppcdn.500px.org/17981845/d1fdacaf5818108c248b05cd472329d314e732d3/3.jpg"/></li>
-    </ul>
-  </div>
-</div>
-	</div><br><BR>-->
-	
-	<!-- banner -->
 
-	<!-- //banner -->
-
-	<!-- top Products -->
+	<!-- top de productos -->
 	<div class="ads-grid py-sm-5 py-4">
 		<div class="container py-xl-4 py-lg-2">
-			<!-- tittle heading -->
+			<!-- titulo de los productos -->
 			<h3 class="tittle-w3l text-center mb-lg-5 mb-sm-4 mb-3">
 				<span>O</span>ur
 				<span>N</span>ew
@@ -137,7 +115,7 @@
 														<input type="hidden" name="currency_code" value="USD" />
 														<input type="hidden" name="return" value=" " />
 														<input type="hidden" name="cancel_return" value=" " />
-														<input type="submit" name="submit" value="Add to cart" class="button btn" />
+														<input type="submit" name="submit" value="Agregar al Carrito" class="button btn" />
 													</fieldset>
 												</form>
 											</div>
@@ -176,7 +154,7 @@
 														<input type="hidden" name="currency_code" value="USD" />
 														<input type="hidden" name="return" value=" " />
 														<input type="hidden" name="cancel_return" value=" " />
-														<input type="submit" name="submit" value="Add to cart" class="button btn" />
+														<input type="submit" name="submit" value="Agregar al Carrito" class="button btn" />
 													</fieldset>
 												</form>
 											</div>
@@ -216,7 +194,7 @@
 														<input type="hidden" name="currency_code" value="USD" />
 														<input type="hidden" name="return" value=" " />
 														<input type="hidden" name="cancel_return" value=" " />
-														<input type="submit" name="submit" value="Add to cart" class="button btn" />
+														<input type="submit" name="submit" value="Agregar al Carrito" class="button btn" />
 													</fieldset>
 												</form>
 											</div>
